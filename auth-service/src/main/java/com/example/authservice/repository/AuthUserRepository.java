00f62eae1038e4a17f4f53c00590bea2b6d0d61f
Saksham Powdel <sakshampowdel@gmail.com>
2026-04-24 18:38:49 -0400

    feat(auth): add @Repository annotation

--- auth-service/src/main/java/com/example/authservice/repository/AuthUserRepository.java
+++ auth-service/src/main/java/com/example/authservice/repository/AuthUserRepository.java
@@ -2,6 +2,8 @@ package com.example.authservice.repository;
 
 import com.example.authservice.model.AuthUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthUserRepository extends MongoRepository<AuthUser, String> {
 }
