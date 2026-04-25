30730348ad2db9dc8ae9367256799de586d07d5c
Saksham Powdel <sakshampowdel@gmail.com>
2026-04-24 21:21:10 -0400

    fix(auth): remove constraints from model and move to dto

--- auth-service/src/main/java/com/example/authservice/model/AuthUser.java
+++ auth-service/src/main/java/com/example/authservice/model/AuthUser.java
@@ -18,32 +18,23 @@ import java.time.Instant;
 @Getter
 @NoArgsConstructor
 public class AuthUser {
-    @Id
-    private String id;
+  @Id private String id;
 
-    @Indexed(unique = true)
-    @Email(message = "Please provide a valid email address")
-    @NotBlank
-    @Setter
-    private String email;
+  @Indexed(unique = true)
+  @Setter
+  private String email;
 
-    @NotBlank
-    @Size(min = 8)
-    @Setter
-    private String passwordHash;
+  @Setter private String passwordHash;
 
-    @Setter
-    private Role role;
+  @Setter private Role role;
 
-    @CreatedDate
-    private Instant createdAt;
+  @CreatedDate private Instant createdAt;
 
-    @LastModifiedDate
-    private Instant updatedAt;
+  @LastModifiedDate private Instant updatedAt;
 
-    public AuthUser(String email, String passwordHash, Role role) {
-        this.email = email;
-        this.passwordHash = passwordHash;
-        this.role = role;
-    }
+  public AuthUser(String email, String passwordHash, Role role) {
+    this.email = email;
+    this.passwordHash = passwordHash;
+    this.role = role;
+  }
 }
