d076f8c07d3fe7bb70c83311fafbda2a1cb323eb
Saksham Powdel <sakshampowdel@gmail.com>
2026-04-28 22:12:39 -0400

    chore(ticket): create mongo config with auditing

---
+++ ticket-service/src/main/java/com/deskflow/ticketservice/config/MongoConfig.java
@@ -0,0 +1,8 @@
+package com.deskflow.ticketservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {}
