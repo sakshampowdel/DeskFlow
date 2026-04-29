dc188eb37ab12b1e43492ff914b3e0ebe6c460df
Saksham Powdel <sakshampowdel@gmail.com>
2026-04-29 19:00:16 -0400

    chore(ticket): add findByReporterId

--- ticket-service/src/main/java/com/deskflow/ticketservice/repository/TicketRepository.java
+++ ticket-service/src/main/java/com/deskflow/ticketservice/repository/TicketRepository.java
@@ -1,8 +1,12 @@
 package com.deskflow.ticketservice.repository;
 
 import com.deskflow.ticketservice.model.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository extends MongoRepository<Ticket, String> {}
+public interface TicketRepository extends MongoRepository<Ticket, String> {
+  Page<Ticket> findByReporterId(String reporterId, Pageable pageable);
+}
