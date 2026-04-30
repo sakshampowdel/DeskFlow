6d05d7447dc620119ef36a9447f5d8355a103344
Saksham Powdel <sakshampowdel@gmail.com>
2026-04-29 21:12:59 -0400

    refactor(ticket): remove redundant extends

--- ticket-service/src/main/java/com/deskflow/ticketservice/repository/TicketRepository.java
+++ ticket-service/src/main/java/com/deskflow/ticketservice/repository/TicketRepository.java
@@ -4,11 +4,9 @@ import com.deskflow.ticketservice.model.Ticket;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository
-    extends MongoRepository<Ticket, String>, QueryByExampleExecutor<Ticket> {
+public interface TicketRepository extends MongoRepository<Ticket, String> {
   Page<Ticket> findByReporterId(String reporterId, Pageable pageable);
 }
